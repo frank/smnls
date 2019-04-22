2c4376c61e06144806718a3a461f5b57330dea65
frank <francidalca@gmail.com>
2019-04-22 18:40:30 +0200

    implemented infer.py

--- src/infer.py
+++ src/infer.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import nltk
 import torch
@@ -12,7 +13,11 @@ from classifier import MLPClassifier
 torch.set_default_tensor_type(torch.cuda.FloatTensor)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+# Parameters
 model_types = ['baseline', 'lstm', 'bilstm', 'maxbilstm']
+outcome = {0: "contradiction",
+           1: "entailment",
+           2: "neutral"}
 
 
 # Nltk tokenizer
@@ -28,8 +33,32 @@ def get_sentences(path):
     return sentences
 
 
-def get_batch(sentences):
-    pass
+def get_batch(sentences, vocab):
+    # tokenize the sentences
+    tokenized = [tokenizer(sentence) for sentence in sentences]
+
+    # get useful parameters
+    n_sentences = len(sentences)
+    lengths = [len(tok) for tok in tokenized]
+    max_len = max(lengths)
+
+    # initialize batch elements
+    batch = torch.zeros(max_len, n_sentences, 300)
+    batch_lens = torch.tensor(lengths)
+
+    # produce embeddings
+    for n in range(n_sentences):
+        for l, word in enumerate(tokenized[n]):
+            batch[l, n] = vocab[word]
+
+    return batch, batch_lens
+
+
+def load_model(encoder, classifier, model_path):
+    model = torch.load(model_path)
+    encoder.load_state_dict(model['encoder_state_dict'])
+    classifier.load_state_dict(model['model_state_dict'])
+    return encoder, classifier
 
 
 def infer():
@@ -44,13 +73,59 @@ def infer():
     # both should be tuples. [0] is a torch.tensor of size (max_len x N x 300)
     #                        [1] is a torch.tensor of size (N)
 
+    # get glove
+    print("GloVe:\t\tloading...", end='\r')
+    glove = torchtext.vocab.GloVe()
+    print("GloVe:\t\tloaded    ")
+
+    # get the batches in the right, embedded format
+    p_batch = get_batch(premises_txt, glove)
+    h_batch = get_batch(hypotheses_txt, glove)
+
+    # remove glove from memory
+    glove = None
+
+    # check that there's an equal number of pairs
+    assert p_batch[1].size() == h_batch[1].size()
+    n_sentences = len(p_batch[1])
+
+    # load the right model
+    model_path = 'models/bad_run/' + model_type + '_model.tar'
+    assert os.path.exists(model_path)
+    print("Using", model_type.upper(), "model")
+
+    if model_type == 'baseline':
+        encoder = Baseline().to(device)
+    elif model_type == 'lstm':
+        encoder = LSTM().to(device)
+    elif model_type == 'bilstm':
+        encoder = BiLSTM().to(device)
+    elif model_type == 'maxbilstm':
+        encoder = BiLSTM(maxpooling=True).to(device)
+    else:
+        encoder = Baseline().to(device)
+
+    classifier = MLPClassifier(encoder, n_sentences).to(device)
+
+    encoder, classifier = load_model(encoder,
+                                     classifier,
+                                     model_path)
+
+    # get predictions
+    y = classifier.forward(p_batch, h_batch)
+    predictions = [outcome[l] for l in y.argmax(1).cpu().numpy()]
+
+    # print result
+    for i in range(n_sentences):
+        print("\nPREMISE:\t", premises_txt[i])
+        print("HYPOTHESIS:\t", hypotheses_txt[i])
+        print("PREDICTION:\t", predictions[i])
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         model_type = sys.argv[1]
     else:
-        print("Unknown model type. Using BASELINE")
         model_type = 'baseline'
     if len(sys.argv) > 2:
         premises_path = sys.argv[2]
@@ -64,4 +139,7 @@ if __name__ == '__main__':
         hypotheses_path = sys.argv[4]
     else:
         hypotheses_path = 'examples/hypotheses.txt'
+    if model_type not in model_types:
+        print("Unknown model type. Using BASELINE")
+        model_type = 'baseline'
     infer()
